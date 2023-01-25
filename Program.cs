8bef587e06018098c4fbb8cf10c0c8b4299315c4
Katya <www@mail.ru>
2023-01-25 15:42:02 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,26 +1,31 @@
-﻿string[] array1 = new string[8] {"123", "23", "hello", "world", "res", "lige","hate","variant"};
-string[] array2 = new string[array1.Length];
+﻿string [] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string [] array2 = new string[array1.Length];
 
-void ShortArray(string[] array1,string[] array2 )
+ void ShortArray(string[] array1,string[] array2 )
 {
     int i = 0;
-    for (int j = 0; i < array1.Length; j++)   
+    int a = 3;
+    for (int j = 0; j < array1.Length; j++)   
     {
-        if (array1[j].Length <= 3)
+        if (array1[j].Length >= a)
         {
-            array2[i] = array1[j];
+            array1[j] = array2[i];
             i++;
         }
-
+       else
+        {
+            j++;
+       }
     }
+ 
 }
 void PrintArray(string[] array)
 {
     for (int j = 0; j < array.Length; j++)
     {
-        Console.Write($"{array[i]}");
+        Console.Write($"{array1[j]}");
     }
     Console.WriteLine();
 }
-ShortArray(array1, array2);
+ShortArray(array1,array2);
 PrintArray(array2);
